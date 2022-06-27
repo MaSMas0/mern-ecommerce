bd7ffb187bffcd2ca056de275770dd62b9fedebe
Mohamed Abd El-Samie Ahmad Mansour <masmas04@gmail.com>
2022-06-27 22:37:38 +0200

    make a home page for products

---
+++ front-end/src/pages/Home.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Row,Col } from 'react-bootstrap';
+import Product  from "../components/Product";
+
+function Home({products}) {
+  return (
+    <>
+   <h1>latest products</h1>
+   <Row>
+    {products.map((product)=> (
+        <Col sm={12} md={6} lg={4} xl={3}>
+            <Product product={product} />
+        </Col>
+    ))}
+   </Row>
+   </>
+  )
+}
+
+export default Home
