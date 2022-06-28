1d8969e52d37004e9a4ab49b699ec53330b42a01
Mohamed Abd El-Samie Ahmad Mansour <masmas04@gmail.com>
2022-06-28 18:14:19 +0200

    connect the MongoDB database with nodeJS and inject it with data

--- back-end/data/users.js
+++ back-end/data/users.js
@@ -1,21 +1,22 @@
-import bcrypt from 'bcryptjs'
-
+import bcrypt from "bcryptjs";
 
 const users = [
-    {
-        name: 'Admin User',
-        email: 'admin@example.com',
-        password: bcrypt.hashSync('123456789', 10),
-        isAdmin: true
-    },
-    {
-        name: 'John Doe',
-        password: bcrypt.hashSync('123456789', 10),
-    },
-    {
-        name: 'jane Doe',
-        password: bcrypt.hashSync('123456789', 10),
-    },
-]
+  {
+    name: "Admin User",
+    email: "admin@example.com",
+    password: bcrypt.hashSync("123456789", 10),
+    isAdmin: true,
+  },
+  {
+    name: "John Doe",
+    email: "john@example.com",
+    password: bcrypt.hashSync("123456789", 10),
+  },
+  {
+    name: "jane Doe",
+    email: "jane@example.com",
+    password: bcrypt.hashSync("123456789", 10),
+  },
+];
 
-export default users
+export default users;
--- back-end/server.js
+++ back-end/server.js
@@ -1,37 +1,36 @@
-import express from 'express'
-import colors from 'colors'
-import products from './data/products.js'
+import express from "express";
+import colors from "colors";
+import products from "./data/products.js";
 import connectDB from "./config/db.js";
-import cors from 'cors'
-import dotenv from 'dotenv'
+import cors from "cors";
+import dotenv from "dotenv";
 
-dotenv.config()
+dotenv.config();
 
-connectDB()
+connectDB();
 
-const app = express()
-const port = process.env.PORT || 3001
+const app = express();
+const port = process.env.PORT || 3001;
 
-app.use(cors())
+app.use(cors());
 
-app.get('/', (req,res)=>{
-    res.send('Hello from server side')
-})
+app.get("/", (req, res) => {
+  res.send("Hello from server side");
+});
 
+app.get("/api/products", (req, res) => {
+  res.json(products);
+});
 
-app.get('/api/products', (req,res)=>{
-    res.json(products)
-})
+app.get("/api/products/:id", (req, res) => {
+  const product = products.find((p) => p._id === req.params.id);
+  res.json(product);
+});
 
-
-app.get('/api/products/:id', (req,res)=>{
-    const product = products.find((p)=>p._id === req.params.id)
-    res.json(product)
-})
-
-
-
-app.listen(port,
-    console.log(`Server is up and running in ${process.env.NODE_ENV} on http://localhost:${port}`.yellow)
-)
-
+app.listen(
+  port,
+  console.log(
+    `Server is up and running in ${process.env.NODE_ENV} on http://localhost:${port}`
+      .yellow
+  )
+);
