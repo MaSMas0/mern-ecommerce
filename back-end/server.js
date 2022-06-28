c9504b6f0d7a752199c2249e77b5b37a14f425f8
Mohamed Abd El-Samie Ahmad Mansour <masmas04@gmail.com>
2022-06-28 20:39:53 +0200

    add routes and error middleware

--- back-end/server.js
+++ back-end/server.js
@@ -1,9 +1,10 @@
 import express from "express";
 import colors from "colors";
-import products from "./data/products.js";
 import connectDB from "./config/db.js";
+import {notFound, errorHandler} from './middleware/errorMiddleware.js'
 import cors from "cors";
 import dotenv from "dotenv";
+import productRoutes from './routes/productRoutes.js'
 
 dotenv.config();
 
@@ -18,14 +19,11 @@ app.get("/", (req, res) => {
   res.send("Hello from server side");
 });
 
-app.get("/api/products", (req, res) => {
-  res.json(products);
-});
+app.use('/api/products',productRoutes)
 
-app.get("/api/products/:id", (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
+app.use(notFound)
+app.use(errorHandler)
+
 
 app.listen(
   port,
