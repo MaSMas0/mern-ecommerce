f04db8d8401246f3d288e9d99766b6392c1c8d7c
Mohamed Abd El-Samie Ahmad Mansour <masmas04@gmail.com>
2022-07-14 06:59:12 +0200

    add @redux/toolkit for a better control over states

--- front-end/src/store.js
+++ front-end/src/store.js
@@ -1,13 +1,8 @@
-// import {legacy_createStore as createStore, combineReducers, applyMiddleware} from 'redux'
+
 import { configureStore } from "@reduxjs/toolkit";
 import ProductListReducer from "./reducers/ProductListSlice";
 import ProductDetailsReducer from "./reducers/ProductDetailsSlice";
-// import thunk from 'redux-thunk'
-// import { composeWithDevTools } from 'redux-devtools-extension'
 
-// const reducer = combineReducers({})
-
-// const intialState = {}
 
 const store = configureStore({
   reducer: {
