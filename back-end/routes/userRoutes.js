94d5d4f63553a6e5f25e64306e0a5073b0d70903
Mohamed Abd El-Samie Ahmad Mansour <masmas04@gmail.com>
2022-07-16 23:30:31 +0200

    add update user endpoint and controller

--- back-end/routes/userRoutes.js
+++ back-end/routes/userRoutes.js
@@ -1,10 +1,10 @@
 import { Router } from "express";
-import { authUser,getUserProfile,registerUser } from "../controllers/userController.js";
+import { authUser,getUserProfile,registerUser,updateUserProfile } from "../controllers/userController.js";
 import { auth } from "../middleware/authMiddleware.js";
 const router = Router()
 
 router.post('/login',authUser)
-router.route('/profile').get(auth,getUserProfile)
+router.route('/profile').get(auth,getUserProfile).put(auth,updateUserProfile)
 router.route('/').post(registerUser)
 
 
